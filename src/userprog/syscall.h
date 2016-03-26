9cc1bf991c110dc4809090a886a0cf46becccf6c
Jonathan <jon.nnnnn@rutgers.edu>
2016-03-25 22:24:04 -0400

    last-min changes...again

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -9,7 +9,7 @@ void syscall_init (void);
 struct child_prc { // our code for child process; borrowing thread synchronization stuff
 	int load; //variable type integer called load
 	int pid; //var type integer called processor id, to store pid
-	int status_of; //retains the status, int value, of the pid
+	int status; //retains the status, int value, of the pid
 
 	struct lock wait_on_me; //lock for waiting for safety
 	struct list_elem elem_of_ch; //list of the elements in the processes
