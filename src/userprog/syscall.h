a41df0d58a0bc96c2a65301f144c54c9015b15f8
Shreyas Hirday <shreyashirday@Shreyass-MBP.fios-router.home>
2016-03-25 20:50:24 -0400

    test

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -4,6 +4,7 @@
 void syscall_init (void);
 
 //shreyas methods
+uint32_t *pagedir_create (void);
 bool create (const char *file, unsigned initial_size);
 bool remove (const char *file);
 int  open (const char *file);
